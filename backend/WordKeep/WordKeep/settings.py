52a34c15d9cbb7bf09efa03f00bb58fd36db94cd
Seyran <106960685+NaShAdIR@users.noreply.github.com>
2024-02-17 22:54:23 +0300

    Add files via upload

---
+++ backend/WordKeep/WordKeep/settings.py
@@ -0,0 +1,20 @@
+from web.core.statics import sp
+
+DEBUG = True
+TRACING = True
+STOP_ON_EXCEPTION = False  # works only with DEBUG=False
+
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 80
+
+INSTALL_APPS = [
+
+]
+
+STATIC_FILE_DIRS = {
+
+}
+
+ROOT_URLPATTERNS = [
+
+]
---
+++ backend/WordKeep/run.py
@@ -0,0 +1,5 @@
+from web import Setup
+
+if __name__ == '__main__':
+    setup = Setup()
+    setup.run()
