eaf1aa18aaf65d9ec65b9895d263cbff9d89f108
Davchick <davchick2000ort@gmail.com>
2024-02-23 23:27:42 +0300

    little fix of "nativewind" package

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,20 +2,7 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        secondary: "rgb(var(--color-secondary) / <alpha-value>)",
-        primary: "rgb(var(--color-primary) / <alpha-value>)",
-        warning: "rgb(var(--color-warning) / <alpha-value>)",
-        success: "rgb(var(--color-success) / <alpha-value>)",
-        danger: "rgb(var(--color-danger) / <alpha-value>)",
-        accent: "rgb(var(--color-accent) / <alpha-value>)",
-        border: "rgb(var(--color-border) / <alpha-value>)",
-        text: "rgb(var(--color-text) / <alpha-value>)",
-        bg: "rgb(var(--color-bg) / <alpha-value>)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
-
