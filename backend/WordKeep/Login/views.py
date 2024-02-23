996cf9f1d293030ff1b13929ee72e580907a4554
NaShAdIR <barsegyan1806@gmail.com>
2024-02-23 21:24:30 +0300

    using new view class

--- backend/WordKeep/Login/views.py
+++ backend/WordKeep/Login/views.py
@@ -3,34 +3,41 @@ from web.http import (
     HttpRequest,
     HTTP_STATUS_200
 )
-from web.core import not_found
 from web.core.statics import get_static
+from web.core.view import View
 
-db = {
-    'username': 'Seyran',
-    'password': 'Nads23dsf34123sd'
-}
+cls
+class Login(View):
+    db = {
+        'username': 'Seyran',
+        'password': 'Nads23dsf34123sd'
+    }
 
+    async def login(self, request: HttpRequest) -> HttpResponse:
+        username = request.payload.get('username')
+        password = request.payload.get('password')
+        if (username == self.db['username']
+                and password == self.db['password']):
+            return (
+                HttpResponse(
+                    status=HTTP_STATUS_200,
+                    body=b'Login',
+                )
+            )
 
-async def login(request: HttpRequest) -> HttpResponse:
-    username = request.payload.get('username')
-    password = request.payload.get('password')
-    if (username == db['username']
-            and password == db['password']):
+        login_page_content = await get_static('login/login.html')
         return (
             HttpResponse(
                 status=HTTP_STATUS_200,
-                body=b'Login',
+                body=login_page_content,
+                headers={
+                    'content-type': 'text/html'
+                }
             )
         )
 
-    login_page_content = await get_static('login/login.html')
-    return (
-        HttpResponse(
-            status=HTTP_STATUS_200,
-            body=login_page_content,
-            headers={
-                'content-type': 'text/html'
-            }
-        )
-    )
+    async def view(self, request: HttpRequest) -> HttpResponse:
+        return await self.login(request)
+
+
+login = Login()
--- backend/WordKeep/run.py
+++ backend/WordKeep/run.py
@@ -3,5 +3,3 @@ from web import Setup
 if __name__ == '__main__':
     setup = Setup()
     setup.run()
-
-
